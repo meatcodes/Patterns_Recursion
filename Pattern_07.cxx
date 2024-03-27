f7eb84640eba6b18b951f2ffa99c2b05ff51635b
Yogesh Kumar <107129373+meatcodes@users.noreply.github.com>
2024-03-27 06:25:37 +0530

    Create Pattern_07.cxx

---
+++ Pattern_07.cxx
@@ -0,0 +1,39 @@
+/*
+     n=3
+     *****
+      ***
+       *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern(int n, int row, int col, int spa)
+{
+    if(n==row) return;
+
+    if(spa<row)
+    {
+        cout<<" ";
+        pattern(n, row, col, spa+1);
+    }
+    else if(col<(2*n-2*row-1))
+    {
+        cout<<"*";
+        pattern(n, row, col+1, spa);
+    }
+    else 
+    {
+        cout<<"\n";
+        pattern(n, row+1, 0, 0);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    pattern(n, 0, 0, 0);
+
+    return 0;
+}
